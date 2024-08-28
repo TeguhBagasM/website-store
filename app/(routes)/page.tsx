496710167fc6493284e88e84bcc0fdb45d2a83a5
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-08-28 12:05:01 +0700

    deploy

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanner("269242a0-744a-4ec7-a8b2-00210cbc7f79");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produk Unggulan" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
